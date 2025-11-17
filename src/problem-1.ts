0dd877f1468a5cfcb618917e0a53fe5e1696d7f9
manna179e <mdhadisurrahmanmanna@gmailo.com>
2025-11-17 21:45:39 +0600

    solved problem-3

--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,6 +1,5 @@
 // let value: number|string|boolean;
 
-
 const formatValue = (value: unknown) => {
   if (typeof value === "number") {
     return value * value;
@@ -8,33 +7,33 @@ const formatValue = (value: unknown) => {
     return value.toUpperCase();
   } else if (typeof value === "boolean") {
     return !value;
-  }else{
-    return 
+  } else {
+    return;
   }
 };
 
+const getLength = (input: string | (number[] | string[])) => {
+  if (typeof input === "string") {
+    return input.length;
+  } else if (Array.isArray(input)) {
+    return input.length;
+  } else {
+    return;
+  }
+};
 
-const getLength = (input:string|(number[]|string[]))=>{
-if(typeof input === "string"){
-   return input.length
-}else if(Array.isArray(input)){
-    return input.length
-}else{
-    return
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    (this.name = name), (this.age = age);
+  }
+
+  getDetails() {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
 }
-}
 
-
-class Person{
-    name:string;
-    age:number
-    constructor(name:string,age:number){
-        this.name = name,
-        this.age = age
-    }
-
-    getDetails(){
-
-    }
-}
+const person1 = new Person("Jhone Doe", 22);
+const person2 = new Person('Alice',25)
 
